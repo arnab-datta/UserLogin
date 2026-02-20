c667301d54d9a31e60497298925ae738e9ded48d
Arnab Datta <dattaarnab2013@gmail.com>
2026-02-20 10:00:00 +0530

    userdata update  commit

--- UserLogin/Services/DbService.cs
+++ UserLogin/Services/DbService.cs
@@ -47,7 +47,7 @@ namespace UserLogin.Services
             using var sqlConnection = new MySqlConnection(_connectionString);
             await sqlConnection.OpenAsync();
 
-            using var sqlCommand = new MySqlCommand("sp_UpdateUserData", sqlConnection);
+            using var sqlCommand = new MySqlCommand("sp_UpdateUserBasicInfo", sqlConnection);
 
             sqlCommand.CommandType = CommandType.StoredProcedure;
             sqlCommand.CommandTimeout = 180;
@@ -68,15 +68,18 @@ namespace UserLogin.Services
             using var sqlConnection = new MySqlConnection(_connectionString);
             await sqlConnection.OpenAsync();
 
-            using var sqlCommand = new MySqlCommand("sp_UpdateUserData", sqlConnection);
+            using var sqlCommand = new MySqlCommand("sp_UpdateUserAddress", sqlConnection);
 
             sqlCommand.CommandType = CommandType.StoredProcedure;
             sqlCommand.CommandTimeout = 180;
             sqlCommand.Parameters.AddWithValue("@par_user_id", userId);
-            sqlCommand.Parameters.AddWithValue("@par_first_name", us.first_name);
-            sqlCommand.Parameters.AddWithValue("@par_last_name", us.last_name);
-            sqlCommand.Parameters.AddWithValue("@par_user_email", us.user_email);
-            sqlCommand.Parameters.AddWithValue("@par_phone_no", us.phone_no);
+            sqlCommand.Parameters.AddWithValue("@par_address_id", userId);
+            sqlCommand.Parameters.AddWithValue("@par_line_1", us.user_address.line_1);
+            sqlCommand.Parameters.AddWithValue("@par_line_2", us.user_address.line_2);
+            sqlCommand.Parameters.AddWithValue("@par_city", us.user_address.city);
+            sqlCommand.Parameters.AddWithValue("@par_state", us.user_address.state);
+            sqlCommand.Parameters.AddWithValue("@par_pincode", us.user_address.pincode);
+            sqlCommand.Parameters.AddWithValue("@par_landmark", us.user_address.landmark);
             using var reader = await sqlCommand.ExecuteReaderAsync();
             dt.Load(reader);
             return dt;
